6aa90057e92cb02d09844f783c6bc71482f41d84
나평근 <itbuddy@pineone.com>
2024-07-15 00:49:02 +0900

    Order 도메인
    Validation 추가

--- src/main/java/org/kakaopay/coffee/api/order/Order.java
+++ src/main/java/org/kakaopay/coffee/api/order/Order.java
@@ -1,5 +1,7 @@
 package org.kakaopay.coffee.api.order;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,7 +10,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Order {
 
+    @NotNull
     private Long menuId;
+
+    @NotNull
+    @Positive
     private int quantity;
 
     @Builder
